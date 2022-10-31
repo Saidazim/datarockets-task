9bec12a872ba4cc4a3961f07dc9ca8849bbd2730
Saidazim <saidazim1416@gmail.com>
2022-11-01 03:40:00 +0500

    feat/add mvp functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  public tipForm = new FormGroup({
+    bill: new FormControl(),
+    tipPercentage: new FormControl(15),
+    numOfPeople: new FormControl(),
+  });
+  public tipAmount: number = 0;
+  public total: number = 0;
+  private subscriptions: Subscription = new Subscription();
+
+  ngOnInit(): void {
+    this.subscriptions.add(
+      this.tipForm.valueChanges.subscribe(() => {
+        this.calculateTip();
+      })
+    );
+  }
+
+  public calculateTip(): void {
+    if (this.tipForm.invalid) return; //add another cases as you wish
+
+    const tip = this.tipForm.value;
+    const bill = Number(tip.bill);
+    const tipPercentage = Number(tip.tipPercentage);
+    const numOfPeople = Number(tip.numOfPeople);
+
+    this.tipAmount = ((bill * tipPercentage) / numOfPeople) / 100;
+    this.total = (bill / numOfPeople) + this.tipAmount;
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.unsubscribe()
+  }
 }
